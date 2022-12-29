515bbd445f5c507bcac039f354155c00be0378e3
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2022-12-29 00:32:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Windows 10 Desktop Clone
+
+---
+
+This project recreates the Microsoft Windows 10 desktop for front-end skills training.
+
+![Project screenshot](./img/Screenshot.png)
+
+> ⚠️ This project was developed and will only work in 1366 x 768 resolution
+
+<br/>
+
+## Credits
+
+**Author**: Alberto Frigatto
+
+**Email**: albertofrigatto.comercial@gmail.com
+
+**Linkedin**: [Click Here](https://www.linkedin.com/in/alberto-frigatto-a72022251)
+<br/>
+
+## Start
+
+To start the project just click on `index.html`.
+<br/>
+
+## package dependencies
+
+- jQuery v3.6.5
+- Bootstrap Icons
---
+++ js/main.js
@@ -0,0 +1,84 @@
+$(() => {
+  let desktop = $('.desktop');
+  let listAppsDesktop = [
+    {
+      image: 'Dump',
+      name: 'Lixeira'
+    },
+    {
+      image: 'Chrome',
+      name: 'Google\nChrome'
+    },
+    {
+      image: 'Steam',
+      name: 'Steam'
+    },
+    {
+      image: 'Firefox',
+      name: 'Mozilla\nFirefox'
+    },
+    {
+      image: 'Discord',
+      name: 'Discord'
+    },
+    {
+      image: 'Photoshop',
+      name: 'Photoshop'
+    },
+    {
+      image: 'Whatsapp',
+      name: 'Whatsapp'
+    },
+  ];
+
+  listAppsDesktop.map(app => {
+    desktop.append(`
+      <article class='shortcut'>
+        <div class='shortcut-icon-container'>
+          <img
+            src='./img/apps/${app.image}.png'
+            alt='${app.name}'
+            class='shortcut-app-icon'
+          />
+          <img
+            src='./img/Shortcut_Desktop_Icon.png'
+            alt='ícone de atalho'
+            class='shortcut-arrow-icon'
+          />
+        </div>
+        <p class='shortcut-name'>${app.name}</p>
+      </article>
+    `);
+  });
+
+  let appsTaskbarSection = $('.taskbar .apps');
+  let listAppsTaskbar = ['Explorer', 'Chrome', 'Vscode', 'Discord'];
+
+  listAppsTaskbar.map(app => {
+    appsTaskbarSection.append(`
+      <button type='button' class='app'>
+        <img src='./img/apps/${app}.png' alt='${app}'/>
+      </button>
+    `);
+  });
+
+
+  const loadTime = () => {
+    let date = new Date();
+
+    let hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+    let minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+    let time = `<span>${hours}:${minutes}</span>`;
+
+    let day = date.getDay() < 10 ? `0${date.getDay()}` : date.getDay();
+    let month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let fullDate = `<span>${day}/${month}/${year}</span>`;
+
+    $('#clock-btn').html(`${time} ${fullDate}`);
+    requestAnimationFrame(loadTime);
+  }
+
+  loadTime();
+});
